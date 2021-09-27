3d3d28a18b080515079146087d1a5095e7dccc91
manjeetbhasin <89914656+manjeetbhasin@users.noreply.github.com>
2021-09-27 12:26:10 +0530

    changed

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,11 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(),
@@ -24,186 +22,201 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key}) : super(key: key);
 
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Wrap Demo'),
-
-      ),
-      body: Container (
-      child: Padding(
-      padding: const EdgeInsets.all(10.0),
-
-        child: Wrap(
-          spacing: 10,
+        appBar: AppBar(
+          title: Text('Wrap Demo'),
+        ),
+        body: Container(
+            child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Wrap(
+            spacing: 10,
             runSpacing: 30,
-
-
-
-            children: <Widget> [
-
-
-            Container(
-
-                height:50,
-                width:150,
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(30),
-                  border: Border.all(color: Colors.black) ,
-
-       ),
-              child: Text("Mehul Patel",style: Theme.of(context).textTheme.headline6,
-                textAlign: TextAlign.center,),
-
-            ),
-             // SizedBox(height: 10,width: 5,),
-
+            children: <Widget>[
               Container(
-
-                  height:50,
-                  width:120,
-                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(30),
-                    border: Border.all(color: Colors.black) ,
-                  ),
-                child: Text("Naman",style: Theme.of(context).textTheme.headline6,
-                textAlign: TextAlign.center ,),
-              ),
-             // SizedBox(height: 50,width: 10,),
-              Container(
-
-                height:50,
-                width:100,
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(30),
-                  border: Border.all(color: Colors.black) ,
+                height: 50,
+                width: 150,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  border: Border.all(color: Colors.black),
                 ),
-                child: Text("Nikunj",style: Theme.of(context).textTheme.headline6,
-                  textAlign: TextAlign.center,),
+                child: Text(
+                  "Mehul Patel",
+                  style: Theme.of(context).textTheme.headline6,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              // SizedBox(height: 10,width: 5,),
+
+              Container(
+                height: 50,
+                width: 120,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  border: Border.all(color: Colors.black),
+                ),
+                child: Text(
+                  "Naman",
+                  style: Theme.of(context).textTheme.headline6,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              // SizedBox(height: 50,width: 10,),
+              Container(
+                height: 50,
+                width: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  border: Border.all(color: Colors.black),
+                ),
+                child: Text(
+                  "Nikunj",
+                  style: Theme.of(context).textTheme.headline6,
+                  textAlign: TextAlign.center,
+                ),
               ),
               //SizedBox(height: 50,width: 10,),
               Container(
-
-                height:50,
-                width:210,
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(30),
-                  border: Border.all(color: Colors.black) ,
+                height: 50,
+                width: 210,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  border: Border.all(color: Colors.black),
                 ),
-                child: Text("Bhargav Sejjpal",style: Theme.of(context).textTheme.headline6,
-                  textAlign: TextAlign.center,),
+                child: Text(
+                  "Bhargav Sejjpal",
+                  style: Theme.of(context).textTheme.headline6,
+                  textAlign: TextAlign.center,
+                ),
               ),
               Container(
-
-                height:50,
-                width:150,
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(30),
-                  border: Border.all(color: Colors.black) ,
+                height: 50,
+                width: 150,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  border: Border.all(color: Colors.black),
                 ),
-                child: Text("Devang",style: Theme.of(context).textTheme.headline6,
-                  textAlign: TextAlign.center,),
+                child: Text(
+                  "Devang",
+                  style: Theme.of(context).textTheme.headline6,
+                  textAlign: TextAlign.center,
+                ),
               ),
               Container(
-
-                height:50,
-                width:240,
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(30),
-                  border: Border.all(color: Colors.black) ,
+                height: 50,
+                width: 240,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  border: Border.all(color: Colors.black),
                 ),
-                child: Text("JayeshPansheriya",style: Theme.of(context).textTheme.headline6,
-                  textAlign: TextAlign.center,),
+                child: Text(
+                  "JayeshPansheriya",
+                  style: Theme.of(context).textTheme.headline6,
+                  textAlign: TextAlign.center,
+                ),
               ),
               Container(
-
-                height:80,
-                width:130,
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(30),
-                  border: Border.all(color: Colors.black) ,
+                height: 80,
+                width: 130,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  border: Border.all(color: Colors.black),
                 ),
-                child: Text("Manjeet",style: Theme.of(context).textTheme.headline6,
-                  textAlign: TextAlign.center,),
-              ),
-
-
-              Container(
-
-                height:100,
-                width:220,
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(30),
-                  border: Border.all(color: Colors.black) ,
+                child: Text(
+                  "Manjeet",
+                  style: Theme.of(context).textTheme.headline6,
+                  textAlign: TextAlign.center,
                 ),
-                child: Text("Harsh Shah",style: Theme.of(context).textTheme.headline6,
-                  textAlign: TextAlign.center,),
               ),
 
               Container(
-
-                height:150,
-                width:150,
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(30),
-                  border: Border.all(color: Colors.black) ,
+                height: 100,
+                width: 220,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  border: Border.all(color: Colors.black),
                 ),
-                child: Text("ILDC",style: Theme.of(context).textTheme.headline2,
-                  textAlign: TextAlign.center,),
+                child: Text(
+                  "Harsh Shah",
+                  style: Theme.of(context).textTheme.headline6,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+
+              Container(
+                height: 150,
+                width: 150,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  border: Border.all(color: Colors.black),
+                ),
+                child: Text(
+                  "ILDC",
+                  style: Theme.of(context).textTheme.headline2,
+                  textAlign: TextAlign.center,
+                ),
               ),
               Container(
-
-                height:100,
-                width:350,
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(30),
-                  border: Border.all(color: Colors.black) ,
+                height: 100,
+                width: 350,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  border: Border.all(color: Colors.black),
                 ),
-                child: Text("Rajveer",style: Theme.of(context).textTheme.headline6,
-                  textAlign: TextAlign.center,),
+                child: Text(
+                  "Rajveer",
+                  style: Theme.of(context).textTheme.headline6,
+                  textAlign: TextAlign.center,
+                ),
               ),
               Container(
-
-                height:80,
-                width:100,
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(30),
-                  border: Border.all(color: Colors.black) ,
+                height: 80,
+                width: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  border: Border.all(color: Colors.black),
                 ),
-                child: Text("A",style: Theme.of(context).textTheme.headline6,
-                  textAlign: TextAlign.center,),
+                child: Text(
+                  "A",
+                  style: Theme.of(context).textTheme.headline6,
+                  textAlign: TextAlign.center,
+                ),
               ),
               Container(
-
-                height:80,
-                width:100,
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(30),
-                  border: Border.all(color: Colors.black) ,
+                height: 80,
+                width: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  border: Border.all(color: Colors.black),
                 ),
-                child: Text("B",style: Theme.of(context).textTheme.headline6,
-                  textAlign: TextAlign.center,),
+                child: Text(
+                  "B",
+                  style: Theme.of(context).textTheme.headline6,
+                  textAlign: TextAlign.center,
+                ),
               ),
               Container(
-
-                height:80,
-                width:100,
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(30),
-                  border: Border.all(color: Colors.black) ,
+                height: 80,
+                width: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  border: Border.all(color: Colors.black),
                 ),
-                child: Text("C",style: Theme.of(context).textTheme.headline6,
-                  textAlign: TextAlign.center,),
+                child: Text(
+                  "C",
+                  style: Theme.of(context).textTheme.headline6,
+                  textAlign: TextAlign.center,
+                ),
               ),
-
-              ],
-
-
-        ),
-
-
-
-
-
-
-
+            ],
+          ),
         )));
-
-
   }
 }
